ff58a342937ab7b8f93261a87e6343592be011df
tyraelHqy <752546894@qq.com>
2021-02-23 11:34:48 +0800

    1. 添加logback日志配置 2. 添加多环境配置 3. 封装返回结果，提供状态枚举类

--- imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
@@ -1,14 +1,27 @@
 package com.imooc.user.controller;
 
 import com.imooc.api.controller.user.HelloControllerApi;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.grace.result.ResponseStatusEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController implements HelloControllerApi {
 
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     public Object hello(){
-        return "hello";
+        logger.debug("Debug:Hello~");
+        logger.info("Debug:Hello~");
+        logger.warn("Debug:Hello~");
+
+//        return IMOOCJSONResult.ok("Hello");
+
+        return GraceJSONResult.errorCustom(ResponseStatusEnum.NO_AUTH);
+
+//        return IMOOCJSONResult.errorMsg("您的信息有误～");
     }
 
 }
