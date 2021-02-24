a79a6419578dfcc7a28cbf1f1bdb9933ccc1a112
tyraelHqy <752546894@qq.com>
2021-02-24 10:21:26 +0800

    1. 安装配置整合redis 2. 更新.gitignore文件

--- imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
@@ -4,14 +4,20 @@ import com.imooc.api.controller.user.HelloControllerApi;
 import com.imooc.grace.result.GraceJSONResult;
 import com.imooc.grace.result.IMOOCJSONResult;
 import com.imooc.grace.result.ResponseStatusEnum;
+import com.imooc.utils.RedisOperator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController implements HelloControllerApi {
 
     final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Autowired
+    private RedisOperator redis;
 
     public Object hello(){
         logger.debug("Debug:Hello~");
@@ -23,6 +29,13 @@ public class HelloController implements HelloControllerApi {
 //        return GraceJSONResult.errorCustom(ResponseStatusEnum.NO_AUTH);
 
 //        return IMOOCJSONResult.errorMsg("您的信息有误～");
+    }
+
+    @GetMapping("/redis")
+    public Object redis() {
+        redis.set("age", "18");
+        logger.info(redis.get("age"));
+        return IMOOCJSONResult.ok(redis.get("age"));
     }
 
 }
